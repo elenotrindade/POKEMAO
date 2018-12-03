857a984a3973ba30b822cb1b205b7d2b2bfd16b0
Estevão-Matheus <matheus935@gmail.com>
2018-12-03 17:22:29 -0200

    tentativa de conserto

--- pokemonteste/src/pokemonteste/Locais.java
+++ pokemonteste/src/pokemonteste/Locais.java
@@ -1,27 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    /*
+     * To change this license header, choose License Headers in Project Properties.
+     * To change this template file, choose Tools | Templates
+     * and open the template in the editor.
+     */
 package pokemonteste;
 
-/**
- *
- * @author eleno
- */
-public class Locais {
+import Pokemons.*;
+import java.util.*;
+
+    /**
+     *
+     * @author eleno
+     */
+    public class Locais {
+
+    String Nome;
+    String Tipo;
+    int    ExperienciaPadrao;// atributo pra calcular experiencia obtida por pokemons selvagens da area de farm
+    Batalha batalha; 
+    public Locais(){};
     
-                    String Nome;
-                    String Tipo;
-                    int    ExperienciaPadrao;// atributo pra calcular experiencia obtida por pokemons selvagens da area de farm
-                    Batalha batalha; 
-                    void rodalocal(Treinador x)
-                    {
-                        
-                    }
-                    
-    /* aqui será instanciado todos os locais, como os ginásios e os lugares secundário para farmar xp
-    * atributos basicos: nome, pokemons e está atrelado a batalha, pois aqui ocorrerá as batalhas
-    * os ginasios tem insignias para facilitar o controle de quando o jogo é completo
-    */
+    
+    
+    public void setExperiencia(int exp)
+    {
+        this.ExperienciaPadrao = exp;
+    }
+    public void rodalocal(Treinador X)
+    {
+        
+    }
+   
+  
 }
+        /* aqui será instanciado todos os locais, como os ginásios e os lugares secundário para farmar xp
+        * atributos basicos: nome, pokemons e está atrelado a batalha, pois aqui ocorrerá as batalhas
+        * os ginasios tem insignias para facilitar o controle de quando o jogo é completo
+        */
+
--- pokemonteste/src/pokemonteste/Menu.java
+++ pokemonteste/src/pokemonteste/Menu.java
@@ -4,6 +4,10 @@
  * and open the template in the editor.
  */
 package pokemonteste;
+import Pokemons.*;
+import Pokemons.Pikachu;
+import static java.lang.System.in;
+import java.util.*;
 
 /**
  *
@@ -23,10 +27,102 @@ public class Menu {
         Treinador coach = new Treinador(nome, idade);
     }
     
+    @SuppressWarnings("empty-statement")
     public void rodar() {
         //Aqui ocorrerá todas as chamadas de funções e classe Locais.
         //Caso queira acrescentar funções à essa classe para que seja chamada na main, vá na Classe Tela no pacote Interface, e chame a função 
         // onde está sendo chamada a função rodar() lá, terá um comentário em CapsLock em cima.
+        Treinador coach = new Treinador();
+        Pikachu pikachu = new Pikachu("Pikachu");
+        System.out.println("Você foi designado para treinar um Pikachu, parabéns!!");
+        coach.pokemons.add(pikachu);
+        Scanner teclado = new Scanner (System.in);
         
-    }
+        //Aqui são criados os ginasios e areas de farm
+        int i;
+        ArrayList<Locais> mundo = new ArrayList();
+        //cria primeira area de farm
+        AreaFarm AreaFarm1 = new AreaFarm();
+        ArrayList<Pokemon> pokemonsarea = new ArrayList();
+        Pokemon tangela = new Tangela ("Tangela");
+        for(i=0;i<10;i++)
+        {
+            pokemonsarea.add(tangela);
+        }
+        AreaFarm1.preencheLista(pokemonsarea);
+        mundo.add(AreaFarm1);
+        
+        //Primeiro Ginasio
+        Ginasio Ginasio1 = new Ginasio();
+        Insignia insignia = new Insignia("Cerulean","Eletrico");
+        LiderdeGinasio Mitch = new LiderdeGinasio ("Bruxo",43);
+        ArrayList<Pokemon> pokemonsgim1 = new ArrayList();
+        Pokemon Ratata = new Ratata ("Ratata");
+        Pokemon Fearow = new Fearow ("Fearow");
+        Mitch.addpokemon(Fearow);
+        Mitch.addpokemon(Ratata);
+        Ginasio1.treinadores.add(Mitch);
+        mundo.add(Ginasio1);
+        
+        //Area Farm 2
+        AreaFarm AreaFarm2 = new AreaFarm();
+        ArrayList<Pokemon> pokemonsarea2 = new ArrayList();
+        Pokemon rapid = new Rapidash ("Fast Foward");
+        for(i=0;i<10;i++)
+        {
+            pokemonsarea.add(rapid);
+        }
+        AreaFarm2.preencheLista(pokemonsarea2);
+        mundo.add(AreaFarm2);
+        
+        
+        
+        
+        //ginasio 2
+        
+      
+        Ginasio Ginasio2 = new Ginasio();
+        Insignia insignia2 = new Insignia("Pallet","fogo");
+        LiderdeGinasio Stan = new LiderdeGinasio ("Stannis the Mannis",43);
+        ArrayList<Pokemon> pokemonsgim2 = new ArrayList();
+        Pokemon Victreebel = new Victreebel ("Vic");
+        Pokemon NineTales = new Ninetales ("Tail");
+        Stan.addpokemon(Victreebel);
+        Stan.addpokemon(NineTales);
+        //Ginasio2.addLider(Stan);
+        mundo.add(Ginasio1);
+        
+        
+        //Area Farm 3
+        
+        AreaFarm AreaFarm3 = new AreaFarm();
+        ArrayList<Pokemon> pokemonsarea3 = new ArrayList();
+        Pokemon Magmar = new Magmar ("Magmar");
+        for(i=0;i<10;i++)
+        {
+            pokemonsarea.add(rapid);
+        }
+        AreaFarm3.preencheLista(pokemonsarea2);
+        mundo.add(AreaFarm3);
+        
+        
+        do{
+            System.out.println("1 - Enfrentar Pokemons Selvagens");
+            System.out.println("2 - Avançar pro ginasio");
+            int escolha = teclado.nextInt();
+         switch (escolha){
+             case 1: mundo.get(0).rodalocal(coach);
+             break;
+             case 2: mundo.remove(0);
+             mundo.get(0).rodalocal(coach);
+             break; 
+            default: System.out.println("Escolha Inválida!");
+            
+            
+            
+            
+        }while(mundo.isEmpty()==false);
+        
+         
+       
 }
